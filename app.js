c3a07e901486c45ee9a2c84de4aff4da098d1c27
Marcus Heisters <marcus.heisters@googlemail.com>
2022-09-17 12:12:19 +0200

    ejs setup

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const app = express(); 
 const port = 3000;
+const ejs = require('ejs');
 
+app.set("view engine", "ejs");
 app.get("/", (req, res) => {
-    res.sendFile(__dirname + "/index.html");
+    res.render("list");
 });
 
 
