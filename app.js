c2f23fbb112be2047601b51aa475cedb781df862
Marcus Heisters <marcus.heisters@googlemail.com>
2022-09-17 12:28:57 +0200

    display current date

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 const express = require('express');
-const app = express(); 
+const app = express();
 const port = 3000;
 const ejs = require('ejs');
 
 app.set("view engine", "ejs");
+
+// Add and format curent date 
+var today = new Date();
+var options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' }
+var currentDate = today.toLocaleDateString("en-US", options);
 app.get("/", (req, res) => {
-    res.render("list");
+    res.render("list", {
+        currentDate
+    })
 });
 
 
