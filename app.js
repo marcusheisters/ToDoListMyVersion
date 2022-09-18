22ad59018fce6a2427647708599091386f299646
Marcus Heisters <marcus.heisters@googlemail.com>
2022-09-18 11:37:31 +0200

    Form for adding new items

--- app.js
+++ app.js
@@ -5,11 +5,13 @@ const ejs = require('ejs');
 
 app.set("view engine", "ejs");
 
-// Add and format curent date 
-var today = new Date();
-var options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' }
-var currentDate = today.toLocaleDateString("en-US", options);
 app.get("/", (req, res) => {
+        // Add and format curent date 
+        var today = new Date();
+        var options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+        var currentDate = today.toLocaleDateString("en-US", options);
+        
+        // Render the new list
     res.render("list", {
         currentDate
     })
