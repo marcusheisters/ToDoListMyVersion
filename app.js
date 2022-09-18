4b708486a0c95bf5adfe12ab8d81c0008068f9a1
Marcus Heisters <marcus.heisters@googlemail.com>
2022-09-18 12:26:04 +0200

    Adding new elements via form

--- app.js
+++ app.js
@@ -2,20 +2,35 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const ejs = require('ejs');
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.set("view engine", "ejs");
 
+// Init Itemlist
+var items = ["Cook", "Eat", "Play"];
+// Add and format curent date 
+var today = new Date();
+var options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+var currentDate = today.toLocaleDateString("en-US", options);
+
 app.get("/", (req, res) => {
-        // Add and format curent date 
-        var today = new Date();
-        var options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
-        var currentDate = today.toLocaleDateString("en-US", options);
-        
-        // Render the new list
+
+    // Render the new list
     res.render("list", {
-        currentDate
+        currentDate, items
     })
 });
+
+app.post("/", (req, res) => {
+    // Add item to existing list
+    items.push(req.body.listItem);
+    
+    // Render the new list
+        res.render("list", {
+            currentDate, items
+        })
+})
 
 
 app.listen(port, () => {
