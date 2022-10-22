faffb656645073525c997178ba113ad815df7ed3
Marcus Heisters <marcus.heisters@googlemail.com>
2022-10-22 18:30:39 +0200

    Include Word route

--- app.js
+++ app.js
@@ -8,31 +8,51 @@ app.use(express.static("public"));
 app.set("view engine", "ejs");
 
 // Init Itemlist
-var items = ["Cook", "Eat", "Play"];
+let items = ["Cook", "Eat", "Play"];
+let workItems = [];
 
 var currentDate = getCurrentDate();
 
-// Render the first page view 
-app.get("/", (_req, res) => {
-    currentDate = getCurrentDate();
-    // Render the list
-    res.render("list", {
-        currentDate, items
-    });
-});
-
-// Add item to list and rerender updated list
-app.post("/", (req, res) => {
-    items.push(req.body.listItem);
-    res.render("list", {
-        currentDate, items
-    })
-})
-
-
 app.listen(port, () => {
     console.log("App listens to port " + port);
-})
+});
+processingHomeRoute();
+processingWorkRoute();
+
+
+// defaukt route
+function processingHomeRoute() {
+    app.get("/", (_req, res) => {
+        currentDate = getCurrentDate();
+        // Render the list
+        res.render("list", { listTitle: currentDate, items: items });
+    });
+
+    // Add item to list and rerender updated list
+    app.post("/", (req, res) => {
+        let item = req.body.listItem;
+
+        if (req.body.list === "Work") {
+            workItems.push(item);
+            res.redirect("/work");
+        } else {
+            items.push(req.body.listItem);
+            res.redirect("/");
+        }
+    });
+}
+
+// /Work route
+function processingWorkRoute() {
+    app.get("/work", (req, res) => {
+        res.render("list", { listTitle: "Work", items: workItems });
+    });
+        
+    app.post("/work", (req, res) => {
+        items.push(req.body.listItem);
+        res.redirect("/work");
+    });
+}
 
 function getCurrentDate() {
     var today = new Date();
